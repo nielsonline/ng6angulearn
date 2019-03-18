72cc7114976efa00ab7664f49c6398a47f6d37cc
nielsonline <nielsettema95@gmail.com>
2019-03-18 19:07:55 +0100

    Adding files to github.
    Yess

--- Angulearn/src/app/app-routing.module.ts
+++ Angulearn/src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
 import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
